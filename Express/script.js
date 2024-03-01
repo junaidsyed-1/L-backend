53dffc0b44be30495a591320f9f188018d33906d
junaid <jaanusd79@gmail.com>
2024-03-01 18:22:26 +0530

    done static file setup & done error handling

--- Express/script.js
+++ Express/script.js
@@ -2,16 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// lets use a middleware
-app.use((req, res, next) => {
-    console.log("Hello from middleware")
-    next();
-});
+app.use(express.static('./public'))
 
 // ejs
 app.set("view engine", "ejs");
 
-app.get('/', function (req, res) {
+// Error
+app.get('/error', function (req, res) {
+    throw new Error("Something went wrong")
+})
+
+app.get('/', function (req, res, next) {
     res.render("index");
 });
 
@@ -23,7 +24,16 @@ app.get('/profile', function (req, res) {
 app.get('/profile/:user', function (req, res) {
     console.log(req.params)
     res.send(`<h1>Hello ${req.params.user}</h1>`)
-})
+});
+
+// Error handling page
+app.use(function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+});
 
 app.listen(port, () => {
     console.log("App listening on port: " + port);
