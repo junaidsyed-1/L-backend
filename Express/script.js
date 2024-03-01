00c71e408f7df9619c9fad6f0f21ffe5d009804d
junaid <jaanusd79@gmail.com>
2024-03-01 17:37:16 +0530

    using middleware

--- Express/script.js
+++ Express/script.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+// lets use a middleware
+app.use((req, res, next) => {
+    console.log("Hello from middleware")
+    next();
+})
+
 app.get('/', function (req, res) {
     res.send("Hello backend");
+});
+
+app.get('/profile', function (req, res) {
+    res.send("<h1>Hello from profile</h1>");
 });
 
 app.listen(port, () => {
