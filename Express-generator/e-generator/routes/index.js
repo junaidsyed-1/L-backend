308e77fae346dcb0e75c3e2f7f63fffc099f2007
junaid <jaanusd79@gmail.com>
2024-03-04 12:31:28 +0530

    set,read and delete cookie

--- Express-generator/e-generator/routes/index.js
+++ Express-generator/e-generator/routes/index.js
@@ -7,27 +7,22 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// How to create a session
-router.get('/create', function (req, res) {
-  req.session.ban = true;
-  res.send("Banned");
+// How to set a cookie
+router.get('/cookie', function (req, res) {
+  res.cookie("Name", "Junad");
+  res.send("Cookie has been set!");
 });
 
-// How to read session
+// How to read cookie
 router.get('/read', function (req, res) {
-  if (req.session.ban === true) {
-    res.send("You are banned");
-  } else {
-    res.send("You are not banned");
-  }
+  if (req.cookies.Name) res.send(req.cookies)
+  else res.send("No cookies found")
 });
 
-// How to delete a session
+// How to delete a cookie
 router.get('/delete', function (req, res) {
-  req.session.destroy(function (err) {
-    if (err) throw err;
-    res.send("Ban removed")
-  })
-})
+  res.clearCookie("Name");
+  res.send("Cookie cleared");
+});
 
 module.exports = router;
