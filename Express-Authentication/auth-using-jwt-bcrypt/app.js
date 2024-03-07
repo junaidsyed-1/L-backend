a24a96a15c20c4b94d0e3801f5dd3b0be19d3515
junaid <jaanusd79@gmail.com>
2024-03-07 11:00:15 +0530

    Done with jwt-bcrypt

--- Express-Authentication/auth-using-jwt-bcrypt/app.js
+++ Express-Authentication/auth-using-jwt-bcrypt/app.js
@@ -3,20 +3,18 @@ const connectToMongoDb = require('./config/database');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const auth = require('./middleware/auth');
 const app = express();
 const port = 3000;
 
 connectToMongoDb();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use('/', indexRouter);
-app.use('/users', auth, usersRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log('listening on port' + port);
-})
-
+});
 
 module.exports = app;
--- Express-Authentication/auth-using-jwt-bcrypt/routes/users.js
+++ Express-Authentication/auth-using-jwt-bcrypt/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const { signup, signin } = require('../controllers/userController');
+const auth = require('../middleware/auth');
 var router = express.Router();
 
 
@@ -7,5 +8,8 @@ router.post('/signup', signup);
 
 router.post('/signin', signin);
 
+router.get('/profile', auth, function (req, res) {
+    res.send('Protected Route');
+})
 
 module.exports = router;
